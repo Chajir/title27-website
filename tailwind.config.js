717169bbc82d5d637c40a6fea479e3482bfda89b
Chajir Ale Dargham Rincon <chajirale@Chajirs-MacBook-Pro.local>
2025-04-06 14:47:18 -0400

    Fix Tailwind content paths to include all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{html,js}",  // Ensure everything in the public folder is included
   ],
   theme: {
     extend: {},
